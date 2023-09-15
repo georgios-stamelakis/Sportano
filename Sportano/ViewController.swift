9c66ce694740aa5b2320bdc95a2a665ddc8145f1
Giorgos Stamelakis <georgios.stamelakis@atos.net>
2023-09-15 14:02:02 +0300

    Initial Commit

---
+++ Sportano/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sportano
+//
+//  Created by Georgios Stamelakis on 15/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
